1c0561133e6be48e61f88892ce7bf3ebd6962106
Peer-Lucas Jeske <jeskep@th-brandenburg.de>
2020-05-17 11:55:29 +0200

    declutter

--- map/models.py
+++ map/models.py
@@ -18,8 +18,6 @@ class HelpPoint(models.Model):
     id = models.AutoField(primary_key=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=250)
-    lat = models.FloatField()
-    long = models.FloatField()
     geom = PointField(default="{'type': 'Point', 'coordinates': [0, 0]}")
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
